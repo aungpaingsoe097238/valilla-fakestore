845943d361a104c47c76294d8b9c3c7ab1193d04
Aung Paing soe <aungpaingsoe097238@gmail.com>
2023-01-29 20:26:09 +0630

    to re disabled item btn

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,25 @@
-import { cartBtn, cartCounter, items, total } from "../main";
+import { cartBox, cartBtn, cartCounter, items, total } from "../main";
 
 export const cartCounterUpdate = () => {
   let count = cartCounter[0].innerText;
   // QuerySelectorAll ဆို loop ပက်ကမယ်
   cartCounter.forEach((current) => (current.innerText = parseInt(count) + 1));
+};
+
+// remove cart item
+window.removeCartItem = (event) => {
+  event.target
+    .closest(".item-in-cart")
+    .classList.add("animate__rotateOutDownLeft");
+
+  setTimeout(() => {
+    event.target.closest(".item-in-cart").remove();
+  }, 1000);
+
+  let itemCards  = [...document.querySelectorAll(".item-card")];
+  
+  itemCards.forEach(el => console.log(el) )
+
 };
 
 // cart item total price
@@ -12,7 +28,23 @@ export const costTotal = () => {
     (pv, cv) => pv + parseFloat(cv.innerHTML),
     0
   );
-  total.innerHTML = result;
+  total.innerHTML = result.toFixed(2);
+};
+
+// Check Same Cart Item
+export const isSameItemInCart = (event, itemDetail) => {
+  let cartItems = [...document.querySelectorAll(".cart-item")];
+  let currentCartItems = [];
+
+  cartItems.forEach((item) => {
+    currentCartItems.push(parseInt(item.getAttribute("item-id")));
+  });
+
+  let sameItem = currentCartItems.filter((item) => item == itemDetail.id);
+
+  if (sameItem.length > -1) {
+    event.target.classList.add("disabled");
+  }
 };
 
 window.inc = (event, price) => {
@@ -21,6 +53,26 @@ window.inc = (event, price) => {
   let cartCost = currentCart.querySelector(".cart-cost");
   cartQuantity.valueAsNumber += 1;
   cartCost.innerText = (cartQuantity.valueAsNumber * price).toFixed(2);
+
+  // checkCondition
+  document.querySelector(".btn-dec").classList.remove("disabled");
+
+  // update cart item total
+  costTotal();
+};
+
+window.dec = (event, price) => {
+  let currentCart = event.target.closest(".item-in-cart");
+  let cartQuantity = currentCart.querySelector(".cart-quantity");
+  let cartCost = currentCart.querySelector(".cart-cost");
+  let cartInputValue = (cartQuantity.valueAsNumber -= 1);
+  cartCost.innerText = (cartQuantity.valueAsNumber * price).toFixed(2);
+
+  // checkCondition
+  if (cartInputValue == 1) {
+    document.querySelector(".btn-dec").classList.add("disabled");
+  }
+
   // update cart item total
   costTotal();
 };
@@ -28,9 +80,14 @@ window.inc = (event, price) => {
 // create item in cart
 export const createItemInCart = ({ id, title, price, image }) => {
   const div = document.createElement("div");
-  div.classList.add("item-in-cart");
+  div.classList.add("item-in-cart", "animate__animated");
   div.innerHTML = `
-    <div class="border rounded p-2">
+    <div class="cart-item border rounded p-2 position-relative" item-id="${id}">
+      <div class=" position-absolute top-0 end-0 m-2">
+        <button class="btn btn-outline-danger btn-sm" onclick="removeCartItem(event)">
+          <i class="bi bi-trash-fill pe-none text-danger"></i>
+        </button>
+      </div>
       <div class="">
         <img src="${image}" class="cart-item-img mb-2">
       </div>
@@ -43,11 +100,11 @@ export const createItemInCart = ({ id, title, price, image }) => {
         </div>
         <div class="col-6">
           <div class="input-group input-group-sm">
-            <button class="btn btn-primary" onclick="dec(event,${price})">
+            <button class="btn btn-primary btn-dec " onclick="dec(event,${price})">
               <i class="bi bi-dash pe-none"></i>
             </button>
             <input type="number" class="form-control text-end cart-quantity" value="1" >
-            <button class="btn btn-primary" onclick="inc(event,${price})">
+            <button class="btn btn-primary btn-inc" onclick="inc(event,${price})">
               <i class="bi bi-plus pe-none"></i>
             </button>
           </div>
@@ -75,8 +132,7 @@ export const addToCart = (e) => {
   newImg.style.transition = 1 + "s";
   document.body.append(newImg);
 
-  // Update cart item price total
-  costTotal();
+  isSameItemInCart(e, itemDetail);
 
   // Image Add To Card animation
   setTimeout((_) => {
@@ -95,6 +151,9 @@ export const addToCart = (e) => {
     });
     newImg.remove();
     cartCounterUpdate();
+    // Check same cart item
     createItemInCart(itemDetail);
+    // update cart item total
+    costTotal();
   }, 800);
 };
